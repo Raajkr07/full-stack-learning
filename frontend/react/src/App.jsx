045462b67e1d2aa9277049ab9b169930078a0156
RAJ KUMAR <rk8210032@gmail.com>
2025-10-06 20:28:09 +0530

    modify App.jsx for example UI

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './style/App.css'
+import Demo1 from './example/Demo1'
 
 function App() {
 
   return (
     <>
-      <p className='flex item-center align-center text-black text-9xl font-bold'>
+      <p className='flex item-center text-black text-9xl font-bold'>
         Hello World
       </p>
+      <Demo1/>
     </>
   )
 }
