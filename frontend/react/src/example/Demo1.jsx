b671892dd959687928f7a4b583f57d3b3313fd6e
RAJ KUMAR <rk8210032@gmail.com>
2025-10-06 20:30:06 +0530

    just for example and testing purpose

---
+++ frontend/react/src/example/Demo1.jsx
@@ -0,0 +1,10 @@
+const Name = () => {
+    return (
+        <div>
+            <h1>This is my name:</h1>
+            <p>Hii my name is Raj Kumar</p>
+        </div>
+    )
+}
+
+export default Name;
