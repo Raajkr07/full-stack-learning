e20903ccb29295ed2e3e2c100521813f5d5e9273
RAJ KUMAR <rk8210032@gmail.com>
2025-10-12 23:44:47 +0530

    learn what is hooks in React

---
+++ frontend/react/src/concepts/hooks/AllFunctions.jsx
@@ -0,0 +1,70 @@
+const AllFunctions = () => {
+    return (
+        <>
+            <h1>This is the learning part of hooks in React</h1>
+
+            <p>Basically hooks are the special function in react that helps us to to multiple things like: managing the state, manage side effect and more</p>
+            <li>
+                <h3>Here are sevral types of hook functions</h3>
+                <ul>useEffect</ul>
+                <ul>useState</ul>
+                <ul>useContext</ul>
+                <ul>useRef</ul>
+                <ul>useCallback</ul>
+            </li>
+        </>
+    )
+}
+
+const UseEffect = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
+
+
+const UseState = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
+
+
+const UseContext = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
+
+
+const UseRef = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
+
+
+const UseCallback = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
+
+
+const UseReducer = () => {
+    return (
+        <>
+            <h1>THis is use for managing the state in the webpage</h1>
+        </>
+    )
+}
