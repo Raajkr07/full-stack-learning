165a3c69a739cf0390a545e97eada3a59c570674
RAJ KUMAR <rk8210032@gmail.com>
2025-10-13 19:19:19 +0530

    react-router setup

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import './style/App.css'
 import Demo1 from './example/Demo1'
 import Demo2 from './example/Demo2'
+import Form from './concepts/forms/FormHandling'
+import Local from './concepts/local-storage/LocalStorage'
+import Fetch from './concepts/api/FetchAPI'
+import AppRoutes from './library/react-router-dom/AppRoutes'
 
 function App() {
 
   return (
     <>
+    <AppRoutes/>
       <p className='flex item-center text-black text-9xl font-bold'>
         Hello World
       </p>
       <Demo1/>
       <Demo2/>
+      <Form />
+      <Local />
+      <Fetch/>
     </>
   )
 }
--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+
+  // this is react-router-dom implementation
   <StrictMode>
-    <App />
-  </StrictMode>,
+
+  {/* Strict mode is used for checking and catching the bugs */}
+    <BrowserRouter>
+      
+      {/* This is my main App */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
