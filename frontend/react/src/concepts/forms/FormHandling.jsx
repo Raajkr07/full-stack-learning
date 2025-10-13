b83a3572755f52a17e637b8aa5c4b44c9687d3d9
RAJ KUMAR <rk8210032@gmail.com>
2025-10-13 19:19:40 +0530

    learn form handling basic

--- frontend/react/src/concepts/forms/FormHandling.jsx
+++ frontend/react/src/concepts/forms/FormHandling.jsx
@@ -1,24 +1,22 @@
 const Form = () => {
 
-    const handleSubmit = () => {
-        if (input.Form === "") {
-            alert('please enter the crediantials');
-        }
-
-        
+    const handleSubmit = (e) => {
+        e.preventDefault(e);
+        console.log("form submited");
     }
+
     return (
         <>
-            <form action="">
-                <div>
+            <form action="" onSubmit={(e) => {handleSubmit(e)}} className="style">
                     <input type="text" placeholder="First Name"/>
                     <input type="text" placeholder="Second Name"/>
                     <input type="email" placeholder="example@gmail.com"/>
                     <input type="number" placeholder="contact"/>
 
-                    <button onClick={handleSubmit}>submit</button>
-                </div>
+                    <button>submit</button>
             </form>
         </>
     )
 }
+
+export default Form;
